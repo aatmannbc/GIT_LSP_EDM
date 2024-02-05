69667682a1fe3795389cdebbba855a7bfae72b33
Aatmann B C <aatmann.bc@bison.howard.edu>
2024-02-04 21:52:53 -0500

    trying another push

--- LSP_Project_Assignment_Spring_2024/src/org/howard/edu/lsp/hw2/WordCounter.java
+++ LSP_Project_Assignment_Spring_2024/src/org/howard/edu/lsp/hw2/WordCounter.java
@@ -1,10 +1,30 @@
 package org.howard.edu.lsp.hw2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordCounter {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        try {
+            // Print the current working directory
+            System.out.println("Current Working Directory: " + System.getProperty("user.dir"));
 
-	}
+            // Use a relative path to the file
+            File myObj = new File("words.txt");
+            Scanner myReader = new Scanner(myObj);
 
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
+
