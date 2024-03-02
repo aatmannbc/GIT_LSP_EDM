d3450bc8eb2cacfbd19a66fb7270ac8af14ed8fd
Aatmann B C <aatmann.bc@bison.howard.edu>
2024-03-01 23:42:03 -0500

    final..

--- LSP_spring2024_assignments/src/org/howard/edu/lsp/assignment4/IntegerSet.java
+++ LSP_spring2024_assignments/src/org/howard/edu/lsp/assignment4/IntegerSet.java
@@ -123,8 +123,106 @@ public class IntegerSet {
         if (contains(item)) {
             elements.remove(item);
         }
+        /**
+         * Checks if two sets are equal (contain the same elements).
+         *
+         * @param otherSet The other set to compare with.
+         * @return True if the sets are equal, false otherwise.
+         */
+        public boolean equals(IntegerSet otherSet) {
+            if (size() != otherSet.size()) {
+                return false;
+            }
+            for (int element : elements) {
+                if (!otherSet.contains(element)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /**
+         * Performs the union operation on two sets, returning a new set with the combined elements.
+         * Does not modify the calling set.
+         *
+         * @param otherSet The other set to perform the union with.
+         * @return A new set containing the union of the elements.
+         */
+        public IntegerSet union(IntegerSet otherSet) {
+            List<Integer> combinedElements = new ArrayList<>(elements);
+            for (int element : otherSet.elements) {
+                if (!combinedElements.contains(element)) {
+                    combinedElements.add(element);
+                }
+            }
+            return new IntegerSet(combinedElements);
+        }
+
+        /**
+         * Performs the intersection operation on two sets, returning a new set with the common elements.
+         * Does not modify the calling set.
+         *
+         * @param otherSet The other set to perform the intersection with.
+         * @return A new set containing the intersection of the elements.
+         */
+        public IntegerSet intersect(IntegerSet otherSet) {
+            List<Integer> intersection = new ArrayList<>();
+            for (int element : elements) {
+                if (otherSet.contains(element)) {
+                    intersection.add(element);
+                }
+            }
+            return new IntegerSet(intersection);
+        }
+
+        /**
+         * Performs the difference operation on two sets, returning a new set with elements in the calling set
+         * that are not present in the other set. Does not modify the calling set.
+         *
+         * @param otherSet The other set to perform the difference with.
+         * @return A new set containing the difference of the elements.
+         */
+        public IntegerSet diff(IntegerSet otherSet) {
+            List<Integer> difference = new ArrayList<>(elements);
+            difference.removeIf(element -> otherSet.contains(element));
+            return new IntegerSet(difference);
+        }
+
+        /**
+         * Performs the complement operation on a set, returning a new set containing all integers
+         * not present in the calling set.
+         *
+         * @return A new set containing the complement of the elements.
+         */
+        public IntegerSet complement(IntegerSet universeSet) {
+            if (isEmpty()) {
+                return new IntegerSet(universeSet.elements);
+            }
+            List<Integer> complement = new ArrayList<>(universeSet.elements);
+            complement.removeIf(element -> elements.contains(element));
+            return new IntegerSet(complement);
+        }
+
+        /**
+         * Returns a string representation of the set in curly braces, with elements separated by commas.
+         *
+         * @return A string representation of the set.
+         */
+        @Override
+        public String toString() {
+            if (isEmpty()) {
+                return "{}";
+            }
+            StringBuilder sb = new StringBuilder("{");
+            for (int i = 0; i < elements.size() - 1; i++) {
+                sb.append(elements.get(i)).append(", ");
+            }
+            sb.append(elements.get(elements.size() - 1)).append("}");
+            return sb.toString();
+        }
+
     }
-}
+
     /**
      * Performs the union operation on two sets, modifying the calling set.
      * Adds all elements from the other set that are not already present.
