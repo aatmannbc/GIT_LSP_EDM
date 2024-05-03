11109d3a6d3a94dfde6aaa2fb919fb5102d3b186
Aatmann B C <aatmann.bc@bison.howard.edu>
2024-05-02 20:27:45 -0400

    final commit

--- LSP_spring2024_assignments/src/org/howard/edu/lsp/oopfinal/question3/ShapeFactoryTest.java
+++ LSP_spring2024_assignments/src/org/howard/edu/lsp/oopfinal/question3/ShapeFactoryTest.java
@@ -7,22 +7,22 @@ import java.awt.Shape;
 import org.junit.jupiter.api.Test;
 
 public class ShapeFactoryTest {
-
+//Testing the code
     @Test
     public void testCreateCircle() {
-        Shape circle = ShapeFactory.createShape("circle");
+        Shape circle = ShapeFactory.createShape("Circle");
         assertTrue(circle instanceof Circle);
     }
 
     @Test
     public void testCreateRectangle() {
-        Shape rectangle = ShapeFactory.createShape("rectangle");
+        Shape rectangle = ShapeFactory.createShape("Rectangle");
         assertTrue(rectangle instanceof Rectangle);
     }
 
     @Test
     public void testCreateInvalidShape() {
-        Shape invalidShape = ShapeFactory.createShape("triangle");
+        Shape invalidShape = ShapeFactory.createShape("Octagon");
         assertNull(invalidShape);
     }
 }
